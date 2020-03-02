5710190af47b85d377cd7d16382d949d6dc57769
wyn <k022298@gmail.com>
2020-03-02 23:02:21 +0800

    finish Pitch Perfect

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import AVFoundation
 
-class PlaySoundsViewController: UIViewController {
+ class PlaySoundsViewController: UIViewController {
     @IBOutlet weak var snailButton: UIButton!
     @IBOutlet weak var chipmunkButton: UIButton!
     @IBOutlet weak var rabbitButton: UIButton!
@@ -26,16 +26,35 @@ class PlaySoundsViewController: UIViewController {
     
     enum ButtonType: Int { case slow = 0, fast, chipmunk, vader, echo, reverb }
     @IBAction func playSoundForButton(_ sender: UIButton){
-        print("play sound button pressed")
+        switch(ButtonType(rawValue: sender.tag)!){
+        case .slow:
+            playSound(rate: 0.5)
+        case .fast:
+            playSound(rate: 1.5)
+        case .chipmunk:
+            playSound(pitch: 1000)
+        case .vader:
+            playSound(pitch: -1000)
+        case .echo:
+            playSound(echo: true)
+        case .reverb:
+            playSound(reverb: true )
+        }
+        configureUI(.playing)
     }
     
     @IBAction func stopButtonPressed(_ sender: AnyObject){
-        print("stop audio  button pressed")
+        stopAudio()
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupAudio()
 
         // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureUI(.notPlaying)
     }
     
 
